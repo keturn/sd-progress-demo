9969d3a5a45b0957f0f2248b7cd0accf054cf327
multimodalart <joaopaulo.passos+multimodal@gmail.com>
2022-08-22 21:25:34 +0000

    Layout update (#7)
    - Layout update (9d961cf60d5bae28abbbe52dc003204a07181167)
    
    
    Co-authored-by: Victor M <victor@users.noreply.huggingface.co>

--- app.py
+++ app.py
@@ -96,7 +96,8 @@ css = """
             margin-bottom: 20px;
         }
         .footer {
-            margin-bottom: 25px;
+            margin-bottom: 45px;
+            margin-top: 35px;
             text-align: center;
             border-bottom: 1px solid #e5e5e5;
         }
@@ -163,38 +164,63 @@ examples = [
 with block:
     gr.HTML(
         """
-            <div style="text-align: center;">
-                <div style="display: inline-flex; align-items: center; gap: .8rem; font-size: 1.75rem;">
-                    <svg width="0.65em" height="0.65em" viewBox="0 0 115 115" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="23" height="23" fill="white"/>
-                        <rect y="69" width="23" height="23" fill="white"/>
-                        <rect x="23" width="23" height="23" fill="#AEAEAE"/>
-                        <rect x="23" y="69" width="23" height="23" fill="#AEAEAE"/>
-                        <rect x="46" width="23" height="23" fill="white"/>
-                        <rect x="46" y="69" width="23" height="23" fill="white"/>
-                        <rect x="69" width="23" height="23" fill="black"/>
-                        <rect x="69" y="69" width="23" height="23" fill="black"/>
-                        <rect x="92" width="23" height="23" fill="#D9D9D9"/>
-                        <rect x="92" y="69" width="23" height="23" fill="#AEAEAE"/>
-                        <rect x="115" y="46" width="23" height="23" fill="white"/>
-                        <rect x="115" y="115" width="23" height="23" fill="white"/>
-                        <rect x="115" y="69" width="23" height="23" fill="#D9D9D9"/>
-                        <rect x="92" y="46" width="23" height="23" fill="#AEAEAE"/>
-                        <rect x="92" y="115" width="23" height="23" fill="#AEAEAE"/>
-                        <rect x="92" y="69" width="23" height="23" fill="white"/>
-                        <rect x="69" y="46" width="23" height="23" fill="white"/>
-                        <rect x="69" y="115" width="23" height="23" fill="white"/>
-                        <rect x="69" y="69" width="23" height="23" fill="#D9D9D9"/>
-                        <rect x="46" y="46" width="23" height="23" fill="black"/>
-                        <rect x="46" y="115" width="23" height="23" fill="black"/>
-                        <rect x="46" y="69" width="23" height="23" fill="black"/>
-                        <rect x="23" y="46" width="23" height="23" fill="#D9D9D9"/>
-                        <rect x="23" y="115" width="23" height="23" fill="#AEAEAE"/>
-                        <rect x="23" y="69" width="23" height="23" fill="black"/>
-                    </svg>
-                    <h1 style="font-weight: 900;">Stable Diffusion Spaces</h1>
-                </div>
-                <p style="margin-bottom: 20px;">Stable Diffusion is a state of the art text-to-image model that generates images from a text description. For faster generation and forthcoming API access you can try <a href="http://beta.dreamstudio.ai/" style="text-decoration: underline;" target="_blank">DreamStudio Beta</a></p>
+            <div style="text-align: center; max-width: 736px; margin: 0 auto;">
+              <div
+                style="
+                  display: inline-flex;
+                  align-items: center;
+                  gap: 0.8rem;
+                  font-size: 1.75rem;
+                "
+              >
+                <svg
+                  width="0.65em"
+                  height="0.65em"
+                  viewBox="0 0 115 115"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="23" height="23" fill="white"></rect>
+                  <rect y="69" width="23" height="23" fill="white"></rect>
+                  <rect x="23" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="23" y="69" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="46" width="23" height="23" fill="white"></rect>
+                  <rect x="46" y="69" width="23" height="23" fill="white"></rect>
+                  <rect x="69" width="23" height="23" fill="black"></rect>
+                  <rect x="69" y="69" width="23" height="23" fill="black"></rect>
+                  <rect x="92" width="23" height="23" fill="#D9D9D9"></rect>
+                  <rect x="92" y="69" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="115" y="46" width="23" height="23" fill="white"></rect>
+                  <rect x="115" y="115" width="23" height="23" fill="white"></rect>
+                  <rect x="115" y="69" width="23" height="23" fill="#D9D9D9"></rect>
+                  <rect x="92" y="46" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="92" y="115" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="92" y="69" width="23" height="23" fill="white"></rect>
+                  <rect x="69" y="46" width="23" height="23" fill="white"></rect>
+                  <rect x="69" y="115" width="23" height="23" fill="white"></rect>
+                  <rect x="69" y="69" width="23" height="23" fill="#D9D9D9"></rect>
+                  <rect x="46" y="46" width="23" height="23" fill="black"></rect>
+                  <rect x="46" y="115" width="23" height="23" fill="black"></rect>
+                  <rect x="46" y="69" width="23" height="23" fill="black"></rect>
+                  <rect x="23" y="46" width="23" height="23" fill="#D9D9D9"></rect>
+                  <rect x="23" y="115" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="23" y="69" width="23" height="23" fill="black"></rect>
+                </svg>
+                <h1 style="font-weight: 900; margin-bottom: 7px;">
+                  Stable Diffusion Spaces
+                </h1>
+              </div>
+              <p style="margin-bottom: 20px;">
+                Stable Diffusion is a state of the art text-to-image model that generates
+                images from a text description. For faster generation and forthcoming API
+                access you can try
+                <a
+                  href="http://beta.dreamstudio.ai/"
+                  style="text-decoration: underline;"
+                  target="_blank"
+                  >DreamStudio Beta</a
+                >
+              </p>
             </div>
         """
     )
